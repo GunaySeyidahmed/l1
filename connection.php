d056b89fa2c19a0b54f55d923581181c6748ccc5
GunaySeyidahmed <gunay.seyidehmedqizi@gmail.com>
2019-01-21 14:07:51 -0800

    lh

--- connection.php
+++ connection.php
@@ -1,11 +1,9 @@
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname="mydb";
-
-
-
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -13,43 +11,36 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-
-function insert(){
-
+function insert($name,$surname,$age,$salary){
 	global $conn;
-	$sql = "INSERT INTO test1 (name, surname, age,salary)
-VALUES ('John', 'Doe', 17,89.8)";
+	$sql = "INSERT INTO test1 (name, surname, age,salary) VALUES ($name, $surname, $age, $salary)";
 
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+	if ($conn->query($sql) === TRUE) {
+	    echo "New record created successfully";
+	} else {
+	    echo "Error: " . $sql . "<br>" . $conn->error;
+	}
 }
 
 function select(){
-global $conn;
-$sql = "SELECT name, surname, age FROM test1";
-$result = $conn->query($sql);
+	global $conn;
+	$sql = "SELECT name, surname, age FROM test1";
+	$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "name: " . $row["name"]. " - surname: " . $row["surname"]. " " . $row["age"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	        echo "name: " . $row["name"]. " - surname: " . $row["surname"]. " " . $row["age"]. "<br>";
+	    }
+	} else {
+	    echo "0 results";
+	}
 }
 
 select();
-
-insert();
+insert("Gunay1","Memmedova1",12,22);
 
 $conn->close();
-
-
-
 
 
 ?>
