40a0892e9d4626c681fc6587c3197da3b06ebc62
GunaySeyidahmed <gunay.seyidehmedqizi@gmail.com>
2019-01-21 13:50:56 -0800

    mk

--- connection.php
+++ connection.php
@@ -2,13 +2,30 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname="mydb";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
 
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+
+$sql = "INSERT INTO test1 (name, surname, age,salary)
+VALUES ('John', 'Doe', 17,89.8)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
+
+
+
 ?>
--- p1.php
+++ p1.php
@@ -1,6 +1,6 @@
 
 <?php
-
+@require_once 'connection.php'
 	 if(isset($_POST['crud']))
 		echo $_POST['name'];
 ?>
