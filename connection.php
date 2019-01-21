1c63e088506d06d5e31a3a95b07eacd469e2b54e
GunaySeyidahmed <gunay.seyidehmedqizi@gmail.com>
2019-01-21 13:59:57 -0800

    nw

--- connection.php
+++ connection.php
@@ -13,7 +13,11 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "INSERT INTO test1 (name, surname, age,salary)
+
+function insert(){
+
+	global $conn;
+	$sql = "INSERT INTO test1 (name, surname, age,salary)
 VALUES ('John', 'Doe', 17,89.8)";
 
 if ($conn->query($sql) === TRUE) {
@@ -21,6 +25,26 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+}
+
+function select(){
+global $conn;
+$sql = "SELECT name, surname, age FROM test1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "name: " . $row["name"]. " - surname: " . $row["surname"]. " " . $row["age"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+}
+
+select();
+
+insert();
 
 $conn->close();
 
